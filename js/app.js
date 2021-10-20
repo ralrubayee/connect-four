80dc2e6274e7aa70302635fabeebac933dc260ce
ralrubayee <ralrubayee@gmail.com>
2021-10-20 16:01:32 -0400

    few fixes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let arrC1,arrC2,arrC3,arrC4,arrC5,arrC6,arrC7
+let arrC1,arrC2,arrC3,arrC4,arrC5,arrC6,arrC7,columns,arrR1 ,arrR2,arrR3,arrR4,arrR5,arrR6, rows , columnsToAdd ,columnsTocount
 let turn = 0
 
 /*------------------------ Cached Element References ------------------------*/
@@ -73,13 +73,13 @@ let c7r6 = document.querySelector("c7r6")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-c1.addEventListener("click",drop=>{addDisk(c1)})
-c2.addEventListener("click",drop=>{addDisk(c2)})
-c3.addEventListener("click",drop=>{addDisk(c3)})
-c4.addEventListener("click",drop=>{addDisk(c4)})
-c5.addEventListener("click",drop=>{addDisk(c5)})
-c6.addEventListener("click",drop=>{addDisk(c6)})
-c7.addEventListener("click",drop=>{addDisk(c7)})
+c1.addEventListener("click",drop=>{addDisk(0)})
+c2.addEventListener("click",drop=>{addDisk(1)})
+c3.addEventListener("click",drop=>{addDisk(2)})
+c4.addEventListener("click",drop=>{addDisk(3)})
+c5.addEventListener("click",drop=>{addDisk(4)})
+c6.addEventListener("click",drop=>{addDisk(5)})
+c7.addEventListener("click",drop=>{addDisk(6)})
 
 /*-------------------------------- Functions --------------------------------*/
 init()
@@ -91,87 +91,37 @@ function init(){
   arrC5 = [c5r1, c5r2, c5r3, c5r4, c5r5, c5r6]
   arrC6 = [c6r1, c6r2, c6r3, c6r4, c6r5, c6r6]
   arrC7 = [c7r1, c7r2, c7r3, c7r4, c7r5, c7r6]
+  columnsToAdd = [arrC1,arrC2,arrC3,arrC4,arrC5,arrC6,arrC7]
+  columnsTocount = [[],[],[],[],[],[],[]]
 
   turn = 1
 }
-function addDisk(c){
-  if (c=== c1){
-    if(turn === 1){
-    arrC1[0].classList.add("red")
-    arrC1.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC1[0].classList.add("yellow")
-      arrC1.shift()
-      changeturn()
-    }
-  } else if (c=== c2){
-    if(turn === 1){
-    arrC2[0].classList.add("red")
-    arrC2.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC2[0].classList.add("yellow")
-      arrC2.shift()
-      changeturn()
-    }
-    
-  } else if (c=== c3){
-    if(turn === 1){
-    arrC3[0].classList.add("red")
-    arrC3.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC3[0].classList.add("yellow")
-      arrC3.shift()
-      changeturn()
-    }
-  } else if (c=== c4){
-    if(turn === 1){
-    arrC4[0].classList.add("red")
-    arrC4.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC4[0].classList.add("yellow")
-      arrC4.shift()
-      changeturn()
-    }
-  } else if (c=== c5){
-    if(turn === 1){
-    arrC5[0].classList.add("red")
-    arrC5.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC5[0].classList.add("yellow")
-      arrC5.shift()
-      changeturn()
-    }
-  } else if (c=== c6){
-    if(turn === 1){
-    arrC6[0].classList.add("red")
-    arrC6.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC6[0].classList.add("yellow")
-      arrC6.shift()
-      changeturn()
-    }
-  } else if (c=== c7){
-    if(turn === 1){
-    arrC7[0].classList.add("red")
-    arrC7.shift()
-    changeturn()
-    } else if (turn === 2){
-      arrC7[0].classList.add("yellow")
-      arrC7.shift()
-      changeturn()
-    }
-  }
+function addDisk(i){
+  let c = columnsToAdd[i]
+if(turn === 1){
+  console.log(turn)
+  c[0].classList.add("red")
+  c.shift()
+  columnsTocount[i].push(1)
+  changeturn()
+} else if (turn === 2){
+  c[0].classList.add("yellow")
+  c.shift()
+  columnsTocount[i].push(2)
+  changeturn()
+}
+
 }
 function changeturn(){
+  console.log(columnsTocount)
   if (turn === 1){
     turn =2
+    isWinner()
   } else if (turn === 2){
     turn =1
+    isWinner()
   }
+}
+function isWinner(){
+
 }
